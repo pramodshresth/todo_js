7ffd23daed82721f62138e9f0745843a69e3f49d
Pramod Kumar Shrestha <shresthap608@gmail.com>
2022-06-22 07:03:34 +0545

    sucess

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
+let taskList = [];
+const tasks = document.querySelector("#tasks");
 function addItem(){
-    if(document.querySelector('#newtask input').value.length == 0){
+    const task = document.querySelector('#newtask input').value;
+    if( task.length == 0){
         alert("Please Enter a Task")
     }
-
     else{
-        document.querySelector('#tasks').innerHTML += `
-            <li class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <input type="button" value="Delete" onclick="" id="delete">
-            </li>
-        `;
-        var current_tasks = document.querySelectorAll("#delete");
-        console.log(current_tasks);
-for(var i=0; i<current_tasks.length; i++){
-    current_tasks[i].onclick = function(){
-        this.parentNode.remove();
-    }
-}      
-    }
+      const newTask = {
+        id: Date.now(),
+        title: task,
+        
+      }
+      taskList.push(newTask); 
+      display(newTask);
+  }
 }
 
+
+function deleteItems(id){
+  for(var i in taskList){
+    if(taskList[i].id==id){
+      taskList.splice(i,1);
+      display2();}}
+}
+
+function updateItem(e){
+var n =prompt("Update Task");
+for(var i in taskList){
+  if(taskList[i].id==e){
+    taskList[i].title=n;
+    display2();
+  }
+ }
+}
+
+
+
+function display(newTask){
+  tasks.innerHTML += `
+  <div>
+    ${newTask.title}
+    <span><input type="button" value="Delete" onclick="deleteItems(${newTask.id})" ></span>
+    <span><input type="button" value="Update" onclick="updateItem(${newTask.id})" ></span>
+  </div>
+  `
+}
+
+
+function display2(){
+  console.log(taskList);
+ if(taskList.length==0){
+   tasks.innerHTML = "";
+ }else{
+  tasks.innerHTML = "";
+   taskList.forEach(element =>{
+     tasks.innerHTML += `
+     <div>
+       ${element.title}
+       <span><input type="button" value="Delete" onclick="deleteItems(${element.id})" ></span>
+       <span><input type="button" value="Update" onclick="updateItem(${element.id})" ></span>
+     </div>
+     `
+    },)
+ }
+ }
